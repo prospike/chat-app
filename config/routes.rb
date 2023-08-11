7431605a6ae73ffebb97a3cd579ad55f8f1375d3
prospike <best_diagram22@hotmail.co.jp>
2023-08-11 11:23:59 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  # get 'messages/index' 元々あったもので、いるかもしれないから取っているだけ
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
