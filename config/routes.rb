f88566a91dc5ce625f1164ebcf794e7858bbd7a6
prospike <best_diagram22@hotmail.co.jp>
2023-08-13 10:01:42 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # get 'messages/index' 元々あったもので、いるかもしれないから取っているだけ
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
